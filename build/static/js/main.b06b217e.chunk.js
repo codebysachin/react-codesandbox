2d6cf5642c93c30602f445d55ac4605a924de090
Sachin Gupta <31564045+codebysachin@users.noreply.github.com>
2023-05-12 22:00:20 +0000

    Update 13 files

---
+++ build/static/js/main.b06b217e.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpreact=this.webpackJsonpreact||[]).push([[0],{10:function(e,t,c){"use strict";c.r(t);var n=c(1),r=c(3),i=c.n(r),s=(c(8),c(0));var j=function(){return Object(s.jsx)(s.Fragment,{children:Object(s.jsx)("h1",{children:"Hi from Header file"})})};function a(){return Object(s.jsx)("div",{className:"App",children:Object(s.jsx)(j,{})})}i.a.render(Object(s.jsx)(n.StrictMode,{children:Object(s.jsx)(a,{})}),document.getElementById("root"))},8:function(e,t,c){}},[[10,1,2]]]);
+//# sourceMappingURL=main.b06b217e.chunk.js.map
